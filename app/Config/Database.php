93c5094602d53ba35a3c046d6778f9eb6fb0f506
Leandro <leandroalegregabriel@outlook.com>
2025-06-13 20:36:13 -0300

    chore(config): cambia nombre de la base de datos en Database.php y .env

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => 'root',
         'password'     => '',
-        'database'     => 'RedBullRacing',
+        'database'     => 'alegre_leandro',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
