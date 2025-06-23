6cdb458e99475c4e1baaeccb83df744b711ef288
Leandro <leandroalegregabriel@outlook.com>
2025-06-23 20:09:16 -0300

    feat(routes): agrega rutas protegidas para alta, baja, ver, editar y actualizar usuarios en Admin_Controller; comenta líneas anteriores

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,20 +5,33 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+// Rutas Principales
 $routes->get('/', 'Home::index');
 $routes->get('principal', 'Home::index');
 $routes->get('monoplaza', 'Home::monoplaza');
 $routes->get('pilotos', 'Home::pilotos');
 $routes->get('contacto', 'Home::contacto');
 
-$routes->get('/enviar-form', 'Usuario_Controller::create');
-$routes->post('/enviar-form', 'Usuario_Controller::formValidation');
+// Rutas relacionadas al formulario de registro
+$routes->get('/enviar-form', 'Usuario_Controller::create'); // Muestra el formulario de registro de usuario
+$routes->post('/enviar-form', 'Usuario_Controller::formValidation'); // Procesa y valida el formulario de registro
 
-$routes->get('/login', 'Login_Controller::create');
-$routes->post('/enviarlogin', 'Login_Controller::auth');
-$routes->get('/panel', 'Panel_Controller::index', ['filter' => 'auth']);
-$routes->get('/logout', 'Login_Controller::logout');
+// Rutas de login y autenticación
+$routes->get('/login', 'Login_Controller::create'); // Muestra el formulario de inicio de sesión
+$routes->post('/enviarlogin', 'Login_Controller::auth'); // Procesa las credenciales del usuario
+$routes->get('/panel', 'Panel_Controller::index', ['filter' => 'auth']); // Panel de back/usuario/usuario_logueado.php
+$routes->get('/logout', 'Login_Controller::logout'); // Cierra la sesión del usuario
 
+// Ruta para mostrar el panel admin back/admin/admin_panel.php
 $routes->get('admin', 'Admin_Controller::index', ['filter' => 'authAdmin']);
+
+// Rutas para dar de baja o alta a un usuario funcion darDe... en Controller/Admin_Controller
 $routes->get('admin/baja/(:num)', 'Admin_Controller::darDeBaja/$1', ['filter' => 'authAdmin']);
 $routes->get('admin/alta/(:num)', 'Admin_Controller::darDeAlta/$1', ['filter' => 'authAdmin']);
+
+// Ruta para ver detalles de un usuario back/admin/ver_usuario.php
+$routes->get('admin/ver/(:num)', 'Admin_Controller::verUsuario/$1');
+
+// Rutas para editar y actualizar usuarios back/admin/editar_usuario.php
+$routes->get('admin/editar/(:num)', 'Admin_Controller::editar/$1');
+$routes->post('admin/actualizar/(:num)', 'Admin_Controller::actualizar/$1');
