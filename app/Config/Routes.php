f54fd80ace765aa28a0a9cc901d6d65736e0114f
Leandro <leandroalegregabriel@outlook.com>
2025-06-24 02:00:31 -0300

    feat(routes): agrega rutas para ver y editar perfil del cliente en Panel_Controller

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -22,16 +22,25 @@ $routes->post('/enviarlogin', 'Login_Controller::auth'); // Procesa las credenci
 $routes->get('/panel', 'Panel_Controller::index', ['filter' => 'auth']); // Panel de back/usuario/usuario_logueado.php
 $routes->get('/logout', 'Login_Controller::logout'); // Cierra la sesión del usuario
 
+// Admin_Controller controlador para admin 
 // Ruta para mostrar el panel admin back/admin/admin_panel.php
 $routes->get('admin', 'Admin_Controller::index', ['filter' => 'authAdmin']);
 
+// Admin_Controller controlador para admin 
 // Rutas para dar de baja o alta a un usuario funcion darDe... en Controller/Admin_Controller
 $routes->get('admin/baja/(:num)', 'Admin_Controller::darDeBaja/$1', ['filter' => 'authAdmin']);
 $routes->get('admin/alta/(:num)', 'Admin_Controller::darDeAlta/$1', ['filter' => 'authAdmin']);
 
+// Admin_Controller controlador para admin 
 // Ruta para ver detalles de un usuario back/admin/ver_usuario.php
 $routes->get('admin/ver/(:num)', 'Admin_Controller::verUsuario/$1');
-
 // Rutas para editar y actualizar usuarios back/admin/editar_usuario.php
 $routes->get('admin/editar/(:num)', 'Admin_Controller::editar/$1');
 $routes->post('admin/actualizar/(:num)', 'Admin_Controller::actualizar/$1');
+
+// Panel_Controller Controlador para cliente
+// Rutas para editar y actualizar mi perfil back/usuario/ver_mi_perfil.php
+$routes->get('panel/perfil', 'Panel_Controller::verPerfil');
+// Rutas para editar y actualizar mi perfil back/usuario/editar_perfil.php
+$routes->get('perfil/editar', 'Panel_Controller::editarPerfil');
+$routes->post('perfil/actualizar', 'Panel_Controller::actualizarPerfil');
